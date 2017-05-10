79ffa1d855435ad01bed6930d321895f5f659399
juan camilo salinas <juancamilo2126@hotmail.com>
2017-05-09 19:57:55 -0500

    Se comienza a trabajar en proyecto, se crea pagina de inicio con emal y contraseña

---
+++ app/src/main/java/com/example/probook/proyecto/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.probook.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebBackForwardList;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
